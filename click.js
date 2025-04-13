bcc22c0b4a6eb2436ed3fa5e8ddadf5d1b505e9f
OwenXuUNI <ox@student.unimelb.edu.au>
2025-04-13 23:04:12 +1000

    Add files via upload

---
+++ click.js
@@ -0,0 +1,65 @@
+let words;
+let currentWord = 0;
+let celestialLinks = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  textAlign(LEFT, TOP);
+  textSize(32);
+  words = "CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL celestial CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL CELESTIAL".split(" ");
+  textWrap(WORD);
+}
+
+function draw() {
+  background(0);
+  fill(255);
+  textFont('Shadows Into Light');
+  text("click", 20, 20); 
+  
+
+  let x = 20;
+  let y = 70;
+  let spaceWidth = textWidth(' ');
+  celestialLinks = []; 
+
+  for (let i = 0; i < currentWord; i++) {
+    let word = words[i];
+    let wordWidth = textWidth(word);
+
+    if (x + wordWidth > width - 20) {
+      x = 20;
+      y += textAscent() + textDescent() + 10;
+    }
+
+    if (word === "celestial") {
+      fill(0, 255, 255); 
+      celestialLinks.push({
+        x: x,
+        y: y,
+        w: wordWidth,
+        h: textAscent() + textDescent()
+      });
+    } else {
+      fill(255);
+    }
+
+    text(word, x, y);
+    x += wordWidth + spaceWidth;
+  }
+}
+
+function mousePressed() {
+  for (let link of celestialLinks) {
+    if (
+      mouseX >= link.x && mouseX <= link.x + link.w &&
+      mouseY >= link.y && mouseY <= link.y + link.h
+    ) {
+      window.open("power.html"); 
+      return;
+    }
+  }
+
+  if (currentWord < words.length) {
+    currentWord++;
+  }
+}
---
+++ helloworld.js
@@ -0,0 +1,18 @@
+let circleSize;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  frameRate(24);
+  background(0);
+}
+
+
+function draw() {
+  r= random(255);
+  g= random(255);
+  b = random(255);
+  
+  fill(r,g,b, 20);
+  textSize(100);
+  text("hello world, this is the end", mouseX, mouseY);
+}
